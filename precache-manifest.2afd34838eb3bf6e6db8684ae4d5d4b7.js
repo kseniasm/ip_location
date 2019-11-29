5109fca69cef31439ce173c44fb58d4654c14b47
Ksyusha <ksenia.smartsev@gmail.com>
2019-11-29 23:18:12 +0200

    Updates

---
+++ precache-manifest.2afd34838eb3bf6e6db8684ae4d5d4b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c660ba2fc720c92f4445aa1a6dcc4d",
+    "url": "/ip_location/index.html"
+  },
+  {
+    "revision": "83d97606a68d8f5695ac",
+    "url": "/ip_location/static/css/main.7cf79a27.chunk.css"
+  },
+  {
+    "revision": "636c38e3459163d2f73a",
+    "url": "/ip_location/static/js/2.4bd54939.chunk.js"
+  },
+  {
+    "revision": "83d97606a68d8f5695ac",
+    "url": "/ip_location/static/js/main.0f4d1125.chunk.js"
+  },
+  {
+    "revision": "81d5e83fee936458430f",
+    "url": "/ip_location/static/js/runtime-main.a42b6c1a.js"
+  }
+]);
